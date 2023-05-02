d10594a6d2aa7f2dc9865743be3d091f4f0369b9
Dj Walker-Morgan <codepope@gmail.com>
2023-05-02 21:37:15 +0100

    Initial Commit

---
+++ QuickDnD Watch App/QuickDnDApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuickDnDApp.swift
+//  QuickDnD Watch App
+//
+//  Created by Dj Walker-Morgan on 02/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct QuickDnD_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ QuickDnD/QuickDnDApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuickDnDApp.swift
+//  QuickDnD
+//
+//  Created by Dj Walker-Morgan on 02/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct QuickDnDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
